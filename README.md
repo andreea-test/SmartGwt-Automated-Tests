1312685942681787e3229d19fe834af7a54eeb29
Andreea Matei <a.matei8@yahoo.com>
2016-12-05 06:31:50 +0200

    Upd: Added logic to search for element matching criteria for exercise 2. Updated readme file:

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SmartGwt-Automated-Tests
 This is an automated test framework that deals with a couple of tests for smartclient, gwt 6.0.0
+It has been created with Serenity, Webdriver and Junit
+
+It is supported by firefox browser versions earlier than 46 and more support is to be added. 
+It can be run as Junit Tests via IDE or through maven with 'mvn clean install'
--- src/test/java/net/smartGwt/pages/DropDownGridPage.java
+++ src/test/java/net/smartGwt/pages/DropDownGridPage.java
@@ -1,12 +1,16 @@
 package net.smartGwt.pages;
 
-import org.openqa.selenium.JavascriptExecutor;
+import java.util.List;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-
 
 @DefaultUrl("http://www.smartclient.com/smartgwt/showcase/#featured_dropdown_grid_category")
 
@@ -20,12 +24,27 @@ public class DropDownGridPage extends PageObject {
 	public void clickOnDropDown() {
 		DropDownTwiddleElement.waitUntilClickable().click();
 	}
-	public void scrollUntilElementIsFound()
-	{	
-		
-		((JavascriptExecutor)getDriver()).executeScript("arguments[0].scrollIntoView();", selection);
 
+	public void scrollUntilElementIsFound() {
+		WebElement firstElementForFocus = getDriver()
+				.findElement(By.xpath(".//table[@id='isc_3Gtable']/tbody/tr[1]/td[1]"));
+		Actions actions = new Actions(getDriver()).moveToElement(firstElementForFocus);
+		final String criterion = ".//table[@id='isc_3Gtable']/tbody/tr/td[1]/div[contains(., 'Exercise')]";
+		List<WebElement> exercises = null;
+		while (exercises == null || exercises.size() == 0) {
+			actions.sendKeys(Keys.PAGE_DOWN).perform();
+			exercises = getDriver().findElements(By.xpath(criterion));
 		}
-	
+		actions.sendKeys(Keys.PAGE_DOWN).perform();
+		exercises = getDriver().findElements(By.xpath(criterion));
+		for (WebElement exercise : exercises) {
+			String unit = exercise.findElement(By.xpath("./../../td[2]/div")).getText();
+			String cost = exercise.findElement(By.xpath("./../../td[3]/div")).getText();
+			if (unit.matches("Ea") && Float.parseFloat(cost) > 1.1f) {
+				exercise.click();
+				return;
+			}
+		}
 	}
 
+}
